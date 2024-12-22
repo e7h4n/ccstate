55806bc819e0d13c2e5daba413aca3a5a258af4b
e7h4n <perfectworks@gmail.com>
2024-12-22 18:42:33 +0800

    feat(solid): add solid hooks

---
+++ packages/solid/src/index.ts
@@ -0,0 +1,5 @@
+export { useGet } from './useGet';
+export { useSet } from './useSet';
+export { useResolved, useLastResolved } from './useResolved';
+export { useLoadable, useLastLoadable } from './useLoadable';
+export { StoreProvider } from './provider';
