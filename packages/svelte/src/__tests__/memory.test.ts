b1b6df819c2c8e527edd124a66cd341256b72f3c
e7h4n <perfectworks@gmail.com>
2024-12-23 15:48:54 +0800

    test: update memory leak test

--- packages/svelte/src/__tests__/memory.test.ts
+++ packages/svelte/src/__tests__/memory.test.ts
@@ -5,6 +5,7 @@ import { render, cleanup, screen } from '@testing-library/svelte';
 import { expect, it } from 'vitest';
 import { computed, getDefaultStore, type Computed } from 'ccstate';
 import Memory from './Memory.svelte';
+import AsyncMemory from './AsyncMemory.svelte';
 
 it('should release memory after view cleanup', async () => {
   let obj$: Computed<{ foo: string }> | undefined = computed(() => {
@@ -20,6 +21,27 @@ it('should release memory after view cleanup', async () => {
   });
 
   expect(screen.getByText('obj: bar')).toBeInTheDocument();
+
+  obj$ = undefined;
+  cleanup();
+
+  expect(await leakDetector.isLeaking()).toBe(false);
+});
+
+it('should release promise memory after view cleanup', async () => {
+  let obj$: Computed<Promise<{ foo: string }>> | undefined = computed(() => {
+    return Promise.resolve({ foo: 'bar' });
+  });
+  const store = getDefaultStore();
+  const leakDetector = new LeakDetector(await store.get(obj$ as Computed<Promise<{ foo: string }>>));
+
+  render(AsyncMemory, {
+    props: {
+      obj$: () => obj$ as Computed<Promise<{ foo: string }>>,
+    },
+  });
+
+  expect(await screen.findByText('obj: bar')).toBeInTheDocument();
 
   obj$ = undefined;
   cleanup();
